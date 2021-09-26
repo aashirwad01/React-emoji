af01b03a7c6d9e6f0e27d3c2d026c8a9c7ac0c7f
AASHIRWAD KUMAR <47081802+aashirwad01@users.noreply.github.com>
2021-09-26 12:36:42 +0530

    feat-app completed database to be increased further

--- src/App.js
+++ src/App.js
@@ -2,26 +2,56 @@ import React, { useState } from "react";
 import "./styles.css";
 
 var emojipedia = {
+  "❤️": "Love",
+  "✨": "Sparkles",
+  "🔥": "Fire",
+  "😊": "Smile",
+  "😔": "sad",
+  "🤠": "Cowboy Hat Face",
   "😀": "Grinning Face",
+  "😠": "Angry Face",
   "😂": "Face with Tears of Joy",
   "🙂": "Slightly Smiling Face",
   "🙃": "Upside-Down Face",
   "😘": "Face Blowing a Kiss",
   "😑": "Expressionless Face",
   "😶": "Face Without Mouth",
-  "😠": "Angry Face",
-  "❤️": "Love",
-  "✨": "Sparkles",
-  "🔥": "Fire",
-  "😊": "Smiling Face with Smiling Eyes",
-  "😂": "Face with Tears of Joy"
+  "😑": "annoyance"
 };
 
+var emojiset = Object.keys(emojipedia);
+
 export default function App() {
+  const [mean, setmean] = useState("");
+
+  function emojihandler(event) {
+    var input = event.target.value;
+    var mean = emojipedia[input];
+    if (mean === undefined) {
+      mean = "We don't have emoji in database yet";
+    }
+    setmean(mean);
+  }
+
+  function inputmean(item) {
+    var mean = emojipedia[item];
+    setmean(mean);
+  }
+
   return (
     <div className="App">
       <h1>What's the Emoji</h1>
-      <input />
+      <input onChange={emojihandler} />
+      <h1>Emoji is : {mean}</h1>
+      <ul>
+        {emojiset.map(function (item) {
+          return (
+            <li onClick={() => inputmean(item)} key={item} id="list">
+              {item}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
