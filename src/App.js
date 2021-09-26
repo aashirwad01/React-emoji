b941a9ac43270a0b6d63b970e87de60f5fe1dbdb
AASHIRWAD KUMAR <47081802+aashirwad01@users.noreply.github.com>
2021-09-26 11:57:51 +0530

    feat-emoji dictionary added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React, { useState } from "react";
 import "./styles.css";
 
+var emojipedia = {
+  "😀": "Grinning Face",
+  "😂": "Face with Tears of Joy",
+  "🙂": "Slightly Smiling Face",
+  "🙃": "Upside-Down Face",
+  "😘": "Face Blowing a Kiss",
+  "😑": "Expressionless Face",
+  "😶": "Face Without Mouth",
+  "😠": "Angry Face",
+  "❤️": "Love",
+  "✨": "Sparkles",
+  "🔥": "Fire",
+  "😊": "Smiling Face with Smiling Eyes",
+  "😂": "Face with Tears of Joy"
+};
+
 export default function App() {
   return (
     <div className="App">
